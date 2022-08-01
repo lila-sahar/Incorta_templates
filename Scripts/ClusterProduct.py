01e8adff599e5411f727eb459ea88819ef0ce292
lila sahar <lsahar@ecapitaladvisors.com>
2022-08-01 15:53:25 -0500

    one hot encoded the clusters, so they can feed into the linear regression!

--- Scripts/ClusterProduct.py
+++ Scripts/ClusterProduct.py
@@ -7,13 +7,12 @@
 # ---------------------------------------------------------------------------
 
 # Imports
-import pandas as pd
-
-# from pyspark.sql import functions as F
+from pyspark.sql import functions as F
 from pyspark.sql.functions import col, sum, count, mean
 from pyspark.ml import Pipeline
+from pyspark.ml.functions import vector_to_array
 from pyspark.ml.clustering import KMeans
-from pyspark.ml.feature import VectorAssembler, StandardScaler
+from pyspark.ml.feature import VectorAssembler, StandardScaler, OneHotEncoder
 from pyspark.ml.evaluation import ClusteringEvaluator
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 
@@ -25,19 +24,18 @@ training = training.groupBy('ProductID', 'ProductName') \
     .agg(sum('Revenue').alias('TotalRevenue'), sum('Cost').alias('TotalCost'))
 
 # Transformers
-## Features
 assembler = VectorAssembler(inputCols = ['TotalRevenue', 'TotalCost'], outputCol = 'unscaledFeatures')
 scaler = StandardScaler(inputCol = 'unscaledFeatures', outputCol = 'features', withStd = True, withMean = False)
 
 ## outlier removal step before or after scaling
 
-# model
+# Model
 kmeans = KMeans().setSeed(1)
 
-# pipeline
+# Pipeline
 pipeline = Pipeline(stages = [assembler, scaler, kmeans])
 
-# tuning
+# Tuning
 paramGrid = ParamGridBuilder() \
     .addGrid(kmeans.k, list(range(3, 10))) \
     .build()
@@ -49,35 +47,21 @@ crossval = CrossValidator(
     numFolds = 3
 )
 
-# model fitting
+# Model Fitting
 cvModel = crossval.fit(training)
 
-# predicting
+# Predicting
 prediction = cvModel.transform(training)
 
-# cleaning output
+# Cleaning Output
 output = prediction.drop('unscaledFeatures', 'features')
 
-# # creation of dummy variable
-# Clusters = output.select('prediction').distinct().rdd.flatMap(lambda x:x).collect()
-# exprs = [F.when(F.col('prediction') == Clusters, 1).otherwise(0).alias(Cluster) for Cluster in Clusters]
-# output.select('ProductID', 'ProductName', *exprs)
+# Feature: Dummy Variable
+encoder = OneHotEncoder(
+    inputCols = ['prediction'],
+    outputCols = ['cluster_vec'])
+output_encoded = encoder.fit(output).transform(output)
 
+result = output_encoded.drop('TotalRevenue', 'TotalCost').withColumnRenamed('prediction', 'Cluster')
 
-# show_dummy = prediction[['ProductID', 'ProductName', 'prediction']]
-# vals_to_replace_cluster = {1: '0', 2: '1', 3: '2'}
-# show_dummy['prediction'] = show_dummy['prediction'].map(vals_to_replace_cluster)
-
-dummy_cluster = pd.get_dummies(output, columns = ['prediction'], prefix = 'Cluster')
-
-output = dummy_cluster.withColumnRenamed('Cluster_2', 'Cluster_3') \
-                      .withColumnRenamed('Cluster_1', 'Cluster_2') \
-                      .withColumnRenamed('Cluster_0', 'Cluster_1')
-
-# column_name = show_dummy.columns.values.tolist()
-# column_name.remove('prediction')
-# show_dummy = show_dummy[column_name].join(dummy_cluster)
-
-save(output)
-
-# this is subject to change depending on the MV
+save(result)
